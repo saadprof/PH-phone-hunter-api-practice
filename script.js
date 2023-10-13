b0d425882b839c12b6c81a4838ae103548b5f735
saadProf <saadprof101@gmail.com>
2023-10-13 21:56:46 +0600

    Display phone from api function added.

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const loadPhoneData = async () =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
     const data = await res.json();
     const phones = data.data;
+    displayPhoneData(phones);
+}
+
+
+//  Display Data
+const displayPhoneData = (phones) =>{
     console.log(phones);
 }
 
