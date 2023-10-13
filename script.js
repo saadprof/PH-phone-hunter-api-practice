79641a7239bb1fb25cfaa4c0b70dc88a3a165124
saadProf <saadprof101@gmail.com>
2023-10-13 21:56:03 +0600

    Phone data loader from api function added.

---
+++ script.js
@@ -0,0 +1,9 @@
+//  Load Data
+const loadPhoneData = async () =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones);
+}
+
+loadPhoneData();
